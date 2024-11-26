e28ef92e0bdee51ec6d9dcb26ac4ca863851ee3d
IlIllllIllIl <jang9@my.centennialcollege.ca>
2024-11-25 21:00:55 -0500

    Update order.js

--- backend/models/order.js
+++ backend/models/order.js
@@ -14,5 +14,13 @@ const orderSchema = new Schema({
     creationDate: { type: Date, required: true }
 });
 
+// Define schema for product model
+const productSchema = new Schema({
+    name: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
+});
+
 //Export the Order model for use in controller
 module.exports = mongoose.model('Order', orderSchema);
+module.exports = mongoose.model('Product', productSchema);
