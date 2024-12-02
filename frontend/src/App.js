fd8c5ab383c90314934113ca61923c529da9ae17
Hunain Aleem <aleemhunain@yahoo.ca>
2024-12-01 20:30:09 -0500

    Front End react components partial
    the first part of react components

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
