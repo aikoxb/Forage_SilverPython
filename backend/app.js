767a18f5b784502ce8705513fe10d1d322536822
IlIllllIllIl <jang9@my.centennialcollege.ca>
2024-12-02 11:14:37 -0500

    user stuff

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,16 @@ const app = express();
 // Middleware to parse incoming JSON requests
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    next();
+  });
+
 //routes for orders
 app.use("/api/orders", ordersRoutes);
 
